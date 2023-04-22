32eef0e64c741040fa94cc1fd74e9f92359f5b74
Ulysses <71786368+ulysseskan@users.noreply.github.com>
2023-04-21 20:37:31 -0700

    If currency is BAT, add current BATUSD price + USD value

--- geminitransfers.py
+++ geminitransfers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Pretty print a list of your last ~8 months of Gemini transfers"""
+"""Pretty print a list of your last ~8 months of Gemini transfers
+   plus current USD value if currency is BAT"""
 import json
 import base64
 import hmac
@@ -34,27 +35,49 @@ response = requests.post(URL, headers=request_headers, timeout=10)
 
 response_json = response.json()
 
+# Get the current price of BAT tokens in USD
+ticker_response = requests.get("https://api.gemini.com/v1/pubticker/batusd", timeout=10)
+ticker_json = ticker_response.json()
+bat_usd_price = float(ticker_json['last'])
+
 if 'result' in response_json and response_json['result'] == 'error':
     print(response_json['message'])
 else:
     my_transfers = response_json
 
     # Specify headers for tabulate
-    headers = ['type', 'status', 'timestamp', 'currency', 'amount', 'destination', 'source']
+    headers = ['type', 'status', 'timestamp', 'USDvalue', 'currency', 'amount', 'source']
 
-    # Create a list to hold the data for tabulate
-    data = []
+    # Create a list to hold the data for the table
+    table_data = []
+
+    BAT_CURRENCY_EXISTS = False
+
     for transfer in my_transfers:
-        timestamp = datetime.datetime.fromtimestamp(transfer['timestampms'] / 1000).strftime('%Y-%m-%d %H:%M')
-        data.append([
+        timestamp = datetime.datetime.fromtimestamp \
+                    (transfer['timestampms'] / 1000).strftime('%Y-%m-%d %H:%M')
+
+        if transfer['currency'] == 'BAT':
+            BAT_CURRENCY_EXISTS = True
+            USD_VALUE = f"${float(transfer['amount']) * bat_usd_price:.2f}"
+        else:
+            USD_VALUE = None  # For other currencies, set the USD value to None
+
+        transfer_data = [
             transfer['type'],
             transfer['status'],
             timestamp,
+            USD_VALUE,
             transfer['currency'],
             transfer['amount'],
-            transfer['destination'],
             transfer['source']
-        ])
+        ]
 
-    # Pretty print the JSON response in columnar spreadsheet style with specific headers
-    print(tabulate(data, headers=headers))
+        table_data.append(transfer_data)
+
+    # Print the current BATUSD price if BAT currency exists
+    if BAT_CURRENCY_EXISTS:
+        print(f"\nCurrent BATUSD price: ${bat_usd_price:.2f}\n")
+
+    # Print the table
+    print(tabulate(table_data, headers=headers))
