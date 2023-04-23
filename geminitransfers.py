f80bcb896e0929534d574475312d2cff3f436ba8
Ulysses <71786368+ulysseskan@users.noreply.github.com>
2023-04-22 19:10:43 -0700

    Add current trading portfolio

--- geminitransfers.py
+++ geminitransfers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Pretty print a list of your last ~8 months of Gemini transfers
-   plus current USD value if currency is BAT"""
+   plus your current trading portfolio"""
 import json
 import base64
 import hmac
@@ -12,6 +12,7 @@ from tabulate import tabulate
 import config
 
 URL = "https://api.gemini.com/v1/transfers"
+BALANCES_URL = "https://api.gemini.com/v1/balances"
 GEMINI_API_KEY = config.GEMINI_API_KEY
 GEMINI_API_SECRET = config.GEMINI_API_SECRET
 
@@ -46,26 +47,22 @@ else:
     my_transfers = response_json
 
     # Specify headers for tabulate
-    headers = ['type', 'status', 'timestamp', 'USDvalue', 'currency', 'amount', 'source']
+    headers = ['type', 'timestamp', 'USDvalue', 'currency', 'amount', 'source']
 
     # Create a list to hold the data for the table
     table_data = []
-
-    BAT_CURRENCY_EXISTS = False
 
     for transfer in my_transfers:
         timestamp = datetime.datetime.fromtimestamp \
                     (transfer['timestampms'] / 1000).strftime('%Y-%m-%d %H:%M')
 
         if transfer['currency'] == 'BAT':
-            BAT_CURRENCY_EXISTS = True
             USD_VALUE = f"${float(transfer['amount']) * bat_usd_price:.2f}"
         else:
             USD_VALUE = None  # For other currencies, set the USD value to None
 
         transfer_data = [
             transfer['type'],
-            transfer['status'],
             timestamp,
             USD_VALUE,
             transfer['currency'],
@@ -75,9 +72,49 @@ else:
 
         table_data.append(transfer_data)
 
-    # Print the current BATUSD price if BAT currency exists
-    if BAT_CURRENCY_EXISTS:
-        print(f"\nCurrent BATUSD price: ${bat_usd_price:.2f}\n")
-
-    # Print the table
+    print("\nLast ~8 Months of Transactions: \n")
     print(tabulate(table_data, headers=headers))
+
+# Get trading balances
+payload_balances = {"request": "/v1/balances", "nonce": payload_nonce + 1}
+encoded_payload_balances = json.dumps(payload_balances).encode()
+b64_balances = base64.b64encode(encoded_payload_balances)
+signature_balances = hmac.new(GEMINI_API_SECRET, b64_balances, hashlib.sha384).hexdigest()
+
+request_headers_balances = {
+    'Content-Type': "text/plain",
+    'Content-Length': "0",
+    'X-GEMINI-APIKEY': GEMINI_API_KEY,
+    'X-GEMINI-PAYLOAD': b64_balances,
+    'X-GEMINI-SIGNATURE': signature_balances,
+    'Cache-Control': "no-cache"
+}
+
+balances_response = requests.post(BALANCES_URL, headers=request_headers_balances, timeout=10)
+balances_json = balances_response.json()
+
+# Process balances and add columns
+balances_table = []
+TOTAL_PORTFOLIO = 0
+
+for balance in balances_json:
+    asset = balance['currency']
+    amount = float(balance['amount'])
+    PRICE = 0
+    if asset == 'BAT':
+        PRICE = bat_usd_price
+    elif asset == 'USD':
+        PRICE = 1
+    value = amount * PRICE
+    TOTAL_PORTFOLIO += value
+    balances_table.append([asset, PRICE, amount, 0])
+
+# Calculate and update Trading Portfolio %
+for row in balances_table:
+    row[3] = (row[2] * row[1]) / TOTAL_PORTFOLIO * 100
+
+print("\nTotal Trading Balances:\n")
+
+# Print the balances table
+print(tabulate(balances_table, \
+               headers=["Asset", "Price", "Balance", "Trading Portfolio %"], floatfmt=".2f"))
